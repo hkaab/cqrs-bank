2ed09a874d85028d414e576e5982752a4d3d2ce4
Hossein Kaabi <112854521+hkaab@users.noreply.github.com>
2025-08-15 07:07:15 +1000

    extend later

--- src/events/account.events.ts
+++ src/events/account.events.ts
@@ -11,12 +11,6 @@ export class MoneyWithdrawnEvent extends Event {
     }
 }
 
-export class MoneyDepositedEvent extends Event {
-    constructor(public readonly accountId: AccountId, public readonly amount: Balance) {
-        super('MoneyDepositedEvent');
-    }
-}
-
 export class InsufficientFundsEvent extends Event {
     constructor(public readonly accountId: AccountId, public readonly amount: Balance, public readonly currentBalance: Balance) {
         super('InsufficientFundsEvent');
